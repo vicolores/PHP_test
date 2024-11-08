e8e99451c21d8357aa17d30d5f6cdd3694be7433
vicolores <151919535+vicolores@users.noreply.github.com>
2024-11-08 23:50:42 +0100

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -19,3 +19,15 @@ function obtenerConexion() {
         die('Error de conexión: ' . $e->getMessage());
     }
 }
+***************************
+<?php
+function conectarBaseDatos($servidor = '127.0.0.1', $usuario = 'mariadb', $contrasena = 'mariadb', $baseDatos = 'mariadb') {
+    $conexion = new mysqli($servidor, $usuario, $contrasena, $baseDatos);
+
+    if ($conexion->connect_error) {
+        die("Error de conexión: " . $conexion->connect_error);
+    }
+
+    return $conexion;
+}
+?>
